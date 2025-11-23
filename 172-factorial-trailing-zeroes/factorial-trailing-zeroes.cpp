fea749fdb3a3a494eb1e3b590006aa51bb6abe52
Swati tripathi <2k24cslo.2414150@gmail.com>
2025-11-23 23:31:53 +0530

    Time: 3 ms (5.55%) | Memory: 8 MB (85.59%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -4,7 +4,7 @@ public:
     int trailingZeroes(int n) {
     int count = 0;
     while (n > 0) {
-        n /= 5;         // counts fives
+        n /= 5;         
         count += n;
     }
     return count;
