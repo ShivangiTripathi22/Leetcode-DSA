bef3f6f057f3d992eaf4698cfdd8923d534aa37b
Swati tripathi <2k24cslo.2414150@gmail.com>
2026-01-20 16:15:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (67.39%) - LeetSync

---
+++ 3636-check-balanced-string/check-balanced-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isBalanced(string num) {
+        int sume = 0;
+        int sumo = 0;
+        for (int i = 0; i < num.size(); i += 2) {
+            sume +=num[i] -'0';
+        }
+        for (int i = 1; i < num.size(); i += 2) {
+            sumo +=num[i]-'0';
+        }
+        if (sume == sumo)
+            return true;
+
+        return false;
+    }
+};
