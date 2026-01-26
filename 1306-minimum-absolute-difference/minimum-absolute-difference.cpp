c9c394add7d967a9d8639aeba5578af4dffe7c42
Swati tripathi <2k24cslo.2414150@gmail.com>
2026-01-26 15:40:03 +0530

    Time: 14 ms (81.53%) | Memory: 36.5 MB (88.73%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -7,14 +7,14 @@ public:
         int mini = INT_MAX;
 
         int i = 1;
-        // First while loop: find minimum difference
+        
         while (i < arr.size()) {
             mini = min(mini, arr[i] - arr[i - 1]);
             i++;
         }
 
         i = 1;
-        // Second while loop: collect pairs
+       
         while (i < arr.size()) {
             if (arr[i] - arr[i - 1] == mini) {
                 ans.push_back({arr[i - 1], arr[i]});
