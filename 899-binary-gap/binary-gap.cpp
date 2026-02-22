906da227a32bb9e3606e1a6a654e5c920382d2d2
Swati tripathi <2k24cslo.2414150@gmail.com>
2026-02-22 22:04:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int binary[32];
+        int i = 0;
+
+        // Convert decimal to binary (LSB first)
+        while (n > 0) {
+            binary[i] = n % 2;
+            n /= 2;
+            i++;
+        }
+
+        int last = -1;
+        int maxGap = 0;
+
+        // Traverse binary array
+        for (int j = 0; j < i; j++) {
+            if (binary[j] == 1) {
+                if (last != -1) {
+                    maxGap = max(maxGap, j - last);
+                }
+                last = j;
+            }
+        }
+
+        return maxGap;
+    }
+};
