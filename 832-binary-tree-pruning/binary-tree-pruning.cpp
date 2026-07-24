6732cb259848e2ab24816296e3d9824e0741232a
Swati tripathi <2k24cslo.2414150@gmail.com>
2026-07-24 10:17:55 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (77.83%) - LeetSync

--- 832-binary-tree-pruning/binary-tree-pruning.cpp
+++ 832-binary-tree-pruning/binary-tree-pruning.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     TreeNode* pruneTree(TreeNode* root) {
-        if(!root)
+        if(root==nullptr)
         return nullptr;
         root->left=pruneTree(root->left);
         root->right=pruneTree(root->right);
-        if (root->val == 0 && root->left == nullptr && root->right == nullptr)
-            return nullptr;
+        if(root->val==0&&root->left==nullptr&&root->right==nullptr)
+         return nullptr;
 
-        return root;
+         return root;
     }
 };
