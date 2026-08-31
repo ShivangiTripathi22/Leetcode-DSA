7d12e8a7ca78aed9c9c9fc91984ff000eeba352c
Swati tripathi <2k24cslo.2414150@gmail.com>
2026-09-01 01:43:58 +0530

    Time: 7 ms (87.84%) | Memory: 20.6 MB (27.18%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -3,12 +3,14 @@ class Solution:
         s=list(s)
         i=0
         j=len(s)-1
+
+        vowel="aeiouAEIOU"
         
         while i<j:
-            if (s[i]!='a'and s[i]!='e'and s[i]!='i'and s[i]!='o'and s[i]!='u'and s[i]!='A'and s[i]!='E'and s[i]!='I'and s[i]!='O'and s[i]!='U'):
+            if s[i] not in vowel:
                 i+=1
 
-            elif(s[j]!='a'and s[j]!='e'and s[j]!='i'and s[j]!='o'and s[j]!='u'and s[j]!='A'and s[j]!='E'and s[j]!='I'and s[j]!='O'and s[j]!='U'):
+            elif s[j] not in vowel:
                 j-=1
 
             else:
